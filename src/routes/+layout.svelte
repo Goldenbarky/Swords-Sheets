4e8422bbc5ce0d77f16361d77933ef04b411c86a
Aven <29378966+TheAvonian@users.noreply.github.com>
2024-04-14 23:23:25 -0400

    The grandmas have returned but theyve been vanquished

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
     import { invalidate } from "$app/navigation";
+    import { user } from "$lib/GenericFunctions.js";
     import { onMount, setContext } from "svelte";
     export let data;
     let { supabase, session } = data;
     $: ({ supabase, session } = data);
 
     onMount(() => {
-        const { data } = supabase.auth.onAuthStateChange((event, _session) => {
-            if (_session?.expires_at !== session?.expires_at) {
-                invalidate("supabase:auth");
+        const { data } = supabase.auth.onAuthStateChange(async (event, _session) => {
+            // console.log(event);
+            if (event === 'SIGNED_IN' || event === 'USER_UPDATED' || event === 'INITIAL_SESSION' || _session?.expires_at !== session?.expires_at) {
+                if (_session) {
+                    $user = _session.user;
+                } else {
+                    $user = null;
+                }
+                // await invalidate("supabase:auth");
             }
         });
 
         return () => data.subscription.unsubscribe();
     });
+
+    // $: console.log(session);
 
     setContext('user', { user: data.user });
 </script>
