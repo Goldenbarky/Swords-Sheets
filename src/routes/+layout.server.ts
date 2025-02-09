58b1fa2bc2ebe932dec1bd7197f101f4fd08ee59
Justin Kennedy <kennedjustin7@gmail.com>
2025-02-09 18:40:17 -0500

    Fixed fetch implemented improperly

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -8,18 +8,18 @@
 
 // src/routes/+layout.server.ts
 import type { LayoutServerLoad } from "./$types";
-export const load: LayoutServerLoad = async ({ url, locals: { supabase, safeGetSession } }) => {
+export const load: LayoutServerLoad = async ({ url, locals: { supabase, safeGetSession }, fetch }) => {
 
     const { session, user } = await safeGetSession();
 
     let spellsCombined;
 
     try {
-        const indexPage = await fetch('127.0.0.1:8080/spells/index.json');
+        const indexPage = await fetch('http://127.0.0.1:8080/spells/index.json');
 
         const files = await indexPage.json();
         const spellFiles = await Promise.all(
-            Object.values(files).map(n => fetch(`127.0.0.1:8080/spells/${n}`))
+            Object.values(files).map(n => fetch(`http://127.0.0.1:8080/spells/${n}`))
         );
         // TEMPORARY ADD ALL KIBBLE ELEMENTAL SPELLS
         const kibblePage = await fetch("https://raw.githubusercontent.com/TheGiddyLimit/homebrew/master/spell/KibblesTasty%3B%20Kibbles'%20Generic%20Elemental%20Spells.json");
@@ -28,8 +28,8 @@ export const load: LayoutServerLoad = async ({ url, locals: { supabase, safeGetS
         const spellJsons = await Promise.all(spellFiles.map(x => x.json()));
 
         spellsCombined = spellJsons.flatMap(x => x['spell']);
-    } catch {
-        console.log("oopsie woopsie - no spells");
+    } catch (e) {
+        console.log("oopsie woopsie - no spells", e);
     }
     
 
